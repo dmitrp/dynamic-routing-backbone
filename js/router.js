708fb8ef51a38f75fdfed6301a7d0c44e8066239
dmitrp <dmitrii.pisotskii@gmail.com>
2016-01-05 18:00:06 +0200

    router code restructuring

--- js/router.js
+++ js/router.js
@@ -2,57 +2,58 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'config'
-], function($, _, Backbone, config) {
+    'config',
+    'header'
+], function ($, _, Backbone, config, header) {
 
-    var initialize = function(){
+    function generateRouterConfig(config) {
+        var routerConfig = {};
 
-        var promise = config.getData();
-
-        promise.success(function(data) {
-
-            var config = data;
-            var router_config = {};
-
-            config.forEach(function (tab) {
-                router_config[tab.id] = tab.order;
-            });
-
-            router_config['*default'] = 'default';
-
-            var AppRouter = Backbone.Router.extend({
-                routes: router_config
-            });
-
-            var app_router = new AppRouter;
-
-            config.forEach(function (tab) {
-
-                var route_string = 'route:' + tab.order;
-                var moduleName = tab.path;
-
-                app_router.on(route_string, function(){
-
-                    require([moduleName], function(view_module){
-                        var projectsView = new view_module();
-                        projectsView.render();
-                    })
-
-                });
-
-            });
-
-            app_router.on('route:default', function(){
-                app_router.navigate(config[0].id, true);
-            });
-
-
-            Backbone.history.start();
+        config.forEach(function (tab) {
+            routerConfig[tab.id] = tab.order;
         });
 
-    };
+        routerConfig['*default'] = 'default';
+
+        return routerConfig;
+    }
+
+    function setRoutes(config, appRouter) {
+        config.forEach(function (tab) {
+            var route_string = 'route:' + tab.order;
+            var moduleName = tab.path;
+
+            appRouter.on(route_string, function () {
+                require([moduleName], function (view_module) {
+                    var projectsView = new view_module();
+                    projectsView.render();
+                })
+            });
+        });
+
+        appRouter.on('route:default', function () {
+            appRouter.navigate(config[0].id, true);
+        });
+    }
+
+    function initHeader() {
+        var headerView = new header();
+        headerView.render();
+    }
+
+    function initialize() {
+        config.getData().success(function (data) {
+            var appRouter = new (Backbone.Router.extend({
+                routes: generateRouterConfig(data)
+            }))();
+
+            setRoutes(data, appRouter);
+            initHeader();
+            Backbone.history.start();
+        });
+    }
+
     return {
         initialize: initialize
     };
 });
-
